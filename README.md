1faf3b0221ced051bf13cf5a49e3ee1bf426f53b
Pete Cook <pete@cookpete.com>
2019-04-16 16:22:17 +0100

    🍺

---
+++ README.md
@@ -0,0 +1,17 @@
+# git-checkout-interactive
+
+Quick switch between git branches.
+
+### Install
+
+```bash
+npm install -g git-checkout-interactive
+```
+
+### Usage
+
+```bash
+gci
+```
+
+![image](https://user-images.githubusercontent.com/1926029/56238297-11153f00-6086-11e9-93b7-fe22800e0056.png)
---
+++ index.js
@@ -0,0 +1,53 @@
+const { spawn } = require('child_process')
+const prompts = require('prompts')
+
+async function run () {
+  const branches = await cmd('git branch -v --sort=-committerdate')
+
+  const choices = branches
+    .split(/\n/)
+    .filter(branch => !!branch.trim())
+    .map(branch => {
+      const [, flag, name, info] = branch.match(/([* ])\s+([^\s]+)\s+(.+)/)
+      return {
+        value: name,
+        disabled: flag === '*',
+        info
+      }
+    })
+
+  const commits = choices.reduce((object, { value, info }) => ({
+    ...object,
+    [value]: info
+  }), {})
+
+  const { branch } = await prompts({
+    type: 'select',
+    name: 'branch',
+    message: 'Select a branch',
+    choices,
+    initial: 0,
+    hint: commits[choices[0].value],
+    warn: 'Current branch',
+    onState ({ value }) {
+      this.hint = commits[value]
+    }
+  })
+
+  await cmd(`git checkout ${branch}`)
+}
+
+function cmd (string) {
+  const [ cmd, ...args ] = string.split(' ')
+  return new Promise((resolve, reject) => {
+    const child = spawn(cmd, args)
+    let data = ''
+    child.stdout.on('data', buffer => {
+      data += buffer.toString()
+    })
+    child.stdout.on('end', () => resolve(data))
+    child.on('error', reject)
+  })
+}
+
+run()
